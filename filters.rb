791a3455551958ed70d9600831c8b5f54da8a493
David Carmichael <carmichael.db88@gmail.com>
2024-02-29 00:02:11 +0000

    Add: method logic as per assignment requirements

--- filters.rb
+++ filters.rb
@@ -1,16 +1,42 @@
 # In this file we define the methods to help filter out candidates
 # This way, we keep these methods separated from other potential parts of the program
 
-def find(id)
-    # Your code Here
+  def find(id)
+    found_candidate = @candidates.find { |candidate| candidate[:id] == id }
   end
   
   def experienced?(candidate)
-    # Your code Here
+    candidate[:years_of_experience] >= 2
   end
   
   def qualified_candidates(candidates)
-    # Your code Here
+    candidates_array = Array.new
+    candidates.each do |candidate|
+      if experienced?(candidate) && github_points?(candidate) && ruby_or_python?(candidate) && applicant_age?(candidate) && applicant_age?(candidate)
+        candidates_array.push(candidate)
+      end
+    end
+    candidates_array
   end
   
   # More methods will go below
+
+  def github_points?(candidate)
+    candidate[:github_points] >= 100
+  end
+
+  def ruby_or_python?(candidate)
+    candidate[:languages].any? {|language| language == "Ruby" || language =="Python"}
+  end
+
+  def application_age?(candidate)
+    (Date.today - candidate[:date_applied]).to_i < 15
+  end
+
+  def applicant_age?(candidate)
+    candidate[:age] > 17
+  end
+
+  def ordered_by_qualifications(candidates)
+    candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]]}
+  end
--- main.rb
+++ main.rb
@@ -6,4 +6,18 @@ require './filters'
 
 ## Your test code can go here
 
-pp @candidates
+# pp @candidates
+# puts experienced?(@candidates[0])
+# puts github_points?(@candidates[0])
+# puts ruby_or_python?(@candidates[0])
+# puts application_age?(@candidates[0])
+# puts applicant_age?(@candidates[0])
+
+# puts experienced?(@candidates[6])
+# puts github_points?(@candidates[4])
+# puts ruby_or_python?(@candidates[2])
+# puts application_age?(@candidates[1])
+# puts applicant_age?(@candidates[6])
+
+# puts qualified_candidates(@candidates)
+# puts ordered_by_qualifications(@candidates)
